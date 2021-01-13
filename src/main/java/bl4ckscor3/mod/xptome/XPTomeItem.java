25ef55cbb09731d8e36d84ebf4762640f47d51cf
bl4ckscor3 <bl4ckscor33@gmail.com>
2021-01-13 10:50:40 +0100

    Revert "fix sound volume and pitch"
    This reverts commit 8d5e8d20afca75c581c1a4950fe07ce6c10b097a.

--- src/main/java/bl4ckscor3/mod/xptome/XPTomeItem.java
+++ src/main/java/bl4ckscor3/mod/xptome/XPTomeItem.java
@@ -93,7 +93,7 @@ public class XPTomeItem extends Item
 			{
 				float pitchMultiplier = player.experienceLevel > 30 ? 1.0F : player.experienceLevel / 30.0F;
 
-				world.playSound(null, player.getPosition(), SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 0.1F, pitchMultiplier * 0.75F);
+				world.playSound(null, player.getPosition(), SoundEvents.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, pitchMultiplier * 0.75F, 1.0F);
 			}
 
 			return new ActionResult<>(ActionResultType.SUCCESS, stack);
