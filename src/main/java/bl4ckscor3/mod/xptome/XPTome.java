8fb672d88beb11d192c8e0f7c30f44217aed858f
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-10-24 16:24:17 +0200

    Readd configuration to change default components
    This comes with a couple of caveats outlined in the config file.

--- src/main/java/bl4ckscor3/mod/xptome/XPTome.java
+++ src/main/java/bl4ckscor3/mod/xptome/XPTome.java
@@ -1,12 +1,13 @@
 package bl4ckscor3.mod.xptome;
 
-import com.mojang.datafixers.util.Unit;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.network.codec.ByteBufCodecs;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.util.ExtraCodecs;
+import net.minecraft.util.Unit;
 import net.minecraft.world.item.CreativeModeTab.TabVisibility;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
@@ -15,8 +16,10 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.event.AnvilUpdateEvent;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -31,16 +34,17 @@ public class XPTome {
 	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> STORED_XP = DATA_COMPONENTS.registerComponentType("stored_xp", builder -> builder.persistent(ExtraCodecs.NON_NEGATIVE_INT).networkSynchronized(ByteBufCodecs.VAR_INT).cacheEncoding());
 	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Integer>> MAXIMUM_XP = DATA_COMPONENTS.registerComponentType("max_xp", builder -> builder.persistent(ExtraCodecs.POSITIVE_INT).networkSynchronized(ByteBufCodecs.VAR_INT).cacheEncoding());
-	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Double>> RETRIEVAL_PERCENTAGE = DATA_COMPONENTS.registerComponentType("retrieval_percentage", builder -> builder.persistent(Codec.DOUBLE.validate(XPTome::validateRetrievalPercentage)).cacheEncoding());
-	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> RETRIEVE_XP_ORBS = DATA_COMPONENTS.registerComponentType("retrieve_xp_orbs", builder -> builder.persistent(Codec.unit(Unit.INSTANCE)).cacheEncoding());
-	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> RETRIEVE_UNTIL_NEXT_LEVEL = DATA_COMPONENTS.registerComponentType("retrieve_until_next_level", builder -> builder.persistent(Codec.unit(Unit.INSTANCE)).cacheEncoding());
-	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> STORE_UNTIL_PREVIOUS_LEVEL = DATA_COMPONENTS.registerComponentType("store_until_previous_level", builder -> builder.persistent(Codec.unit(Unit.INSTANCE)).cacheEncoding());
+	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Double>> RETRIEVAL_PERCENTAGE = DATA_COMPONENTS.registerComponentType("retrieval_percentage", builder -> builder.persistent(Codec.DOUBLE.validate(XPTome::validateRetrievalPercentage)).networkSynchronized(ByteBufCodecs.DOUBLE).cacheEncoding());
+	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> RETRIEVE_XP_ORBS = DATA_COMPONENTS.registerComponentType("retrieve_xp_orbs", builder -> builder.persistent(Unit.CODEC).networkSynchronized(StreamCodec.unit(Unit.INSTANCE)).cacheEncoding());
+	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> RETRIEVE_UNTIL_NEXT_LEVEL = DATA_COMPONENTS.registerComponentType("retrieve_until_next_level", builder -> builder.persistent(Unit.CODEC).networkSynchronized(StreamCodec.unit(Unit.INSTANCE)).cacheEncoding());
+	public static final DeferredHolder<DataComponentType<?>, DataComponentType<Unit>> STORE_UNTIL_PREVIOUS_LEVEL = DATA_COMPONENTS.registerComponentType("store_until_previous_level", builder -> builder.persistent(Unit.CODEC).networkSynchronized(StreamCodec.unit(Unit.INSTANCE)).cacheEncoding());
 	/** @deprecated This is kept for legacy reasons. Use the field below this one. */
 	@Deprecated
 	public static final DeferredItem<OldXPTomeItem> XP_BOOK = ITEMS.register("xp_book", () -> new OldXPTomeItem(new Item.Properties().stacksTo(1)));
-	public static final DeferredItem<XPTomeItem> XP_TOME = ITEMS.register("xp_tome", () -> new XPTomeItem(new Item.Properties().stacksTo(1).component(STORED_XP, 0).component(MAXIMUM_XP, XPTomeItem.DEFAULT_MAX_XP).component(RETRIEVAL_PERCENTAGE, 1.0D)));
+	public static final DeferredItem<XPTomeItem> XP_TOME = ITEMS.register("xp_tome", () -> new XPTomeItem(Configuration.CONFIG.applyToDefaultComponents(new Item.Properties().stacksTo(1).component(STORED_XP, 0))));
 
-	public XPTome(IEventBus modBus) {
+	public XPTome(IEventBus modBus, ModContainer container) {
+		container.registerConfig(ModConfig.Type.STARTUP, Configuration.CONFIG_SPEC, "xptome.toml");
 		DATA_COMPONENTS.register(modBus);
 		ITEMS.register(modBus);
 		modBus.addListener(this::onCreativeModeTabBuildContents);
