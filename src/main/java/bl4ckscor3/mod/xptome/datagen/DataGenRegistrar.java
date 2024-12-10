8af2cdf5d7449a16da1a837f7fb0052db00a0d17
bl4ckscor3 <bl4ckscor33@gmail.com>
2024-12-10 21:08:33 +0100

    Update to 1.21.4

--- src/main/java/bl4ckscor3/mod/xptome/datagen/DataGenRegistrar.java
+++ src/main/java/bl4ckscor3/mod/xptome/datagen/DataGenRegistrar.java
@@ -1,13 +1,9 @@
 package bl4ckscor3.mod.xptome.datagen;
 
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 import bl4ckscor3.mod.xptome.XPTome;
 import net.minecraft.DetectedVersion;
-import net.minecraft.core.HolderLookup;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.PackOutput;
 import net.minecraft.data.metadata.PackMetadataGenerator;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.packs.PackType;
@@ -23,15 +19,11 @@ public class DataGenRegistrar {
 	private DataGenRegistrar() {}
 
 	@SubscribeEvent
-	public static void onGatherData(GatherDataEvent event) {
-		DataGenerator generator = event.getGenerator();
-		PackOutput output = generator.getPackOutput();
-		CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
-
-		generator.addProvider(event.includeServer(), new ItemTagGenerator(output, lookupProvider, event.getExistingFileHelper()));
-		generator.addProvider(event.includeServer(), new RecipeGenerator.Runner(output, lookupProvider));
+	public static void onGatherData(GatherDataEvent.Client event) {
+		event.createProvider(ItemTagGenerator::new);
+		event.createProvider(RecipeGenerator.Runner::new);
 		//@formatter:off
-		generator.addProvider(true, new PackMetadataGenerator(output)
+		event.createProvider(output -> new PackMetadataGenerator(output)
 				.add(PackMetadataSection.TYPE, new PackMetadataSection(Component.literal("XP Tome resources & data"),
 						DetectedVersion.BUILT_IN.getPackVersion(PackType.CLIENT_RESOURCES),
 						Optional.of(new InclusiveRange<>(0, Integer.MAX_VALUE)))));
